5ec555a5960307a498d140cd0838b7b6aabb9213
hawariyaw paulos <hawipaul308@gmail.com>
2024-02-03 12:49:45 -0800

    printingstring

---
+++ src/learnjava1/Main.java
@@ -0,0 +1,15 @@
+package learnjava1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        // in java we store our code inside a class
+		System.out.println("hello world!");
+		System.out.println("   /|");
+		System.out.println("  / |");
+		System.out.println(" /  |");
+		System.out.println("/___|");
+	}
+
+}
